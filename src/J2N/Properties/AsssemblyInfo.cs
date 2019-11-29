6c1ed40953c9db8f77436907192c544284bac91c
Shad Storhaug <shad@shadstorhaug.com>
2019-11-29 20:56:15 +0700

    Added InternalsVisibleTo attribute for tests

--- src/J2N/Properties/AsssemblyInfo.cs
+++ src/J2N/Properties/AsssemblyInfo.cs
@@ -1,3 +1,7 @@
-﻿using System;
+﻿using J2N;
+using System;
+using System.Runtime.CompilerServices;
 
 [assembly: CLSCompliant(true)]
+
+[assembly: InternalsVisibleTo("J2N.Tests, PublicKey=" + AssemblyKeys.PublicKey)]
