057c56c9ca24635ce20b330c249e60717c74a0dc
Shad Storhaug <shad@shadstorhaug.com>
2019-12-02 02:24:30 +0700

    J2N.TestFramework: Added RandomExtensions class

---
+++ src/J2N.TestFramework/RandomExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace J2N
+{
+    /// <summary>
+    /// Extensions to <see cref="System.Random"/>.
+    /// </summary>
+    public static class RandomExtensions
+    {
+        /// <summary>
+        /// Generates a random <see cref="bool"/>, with a random distribution of
+        /// approximately 50/50.
+        /// </summary>
+        /// <param name="random">This <see cref="Random"/>.</param>
+        /// <returns>A random <see cref="bool"/>.</returns>
+        public static bool NextBoolean(this Random random)
+        {
+            return random.Next(1, 100) >= 50;
+        }
+    }
+}
