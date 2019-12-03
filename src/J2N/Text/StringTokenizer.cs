e403ef4f7ceb08a6c3d9c1d62c19d21ed0c6500f
Shad Storhaug <shad@shadstorhaug.com>
2019-12-03 20:48:34 +0700

    Added StringTokenizer + tests

---
+++ src/J2N/Text/StringTokenizer.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace J2N.Text
+{
+    /// <summary>
+    /// The <see cref="StringTokenizer"/> class allows an application to break a string
+    /// into tokens by performing code point comparison. The <see cref="StringTokenizer"/>
+    /// methods do not distinguish among identifiers, numbers, and quoted strings,
+    /// nor do they recognize and skip comments.
+    /// </summary>
+    /// <remarks>
+    /// The set of delimiters (the codepoints that separate tokens) may be specified
+    /// either at creation time or on a per-token basis.
+    /// <para/>
+    /// An instance of <see cref="StringTokenizer"/> behaves in one of three ways,
+    /// depending on whether it was created with the <c>returnDelimiters</c> flag
+    /// having the value <c>true</c> or <c>false</c>:
+    /// <list type="bullet">
+    ///     <item><description>If <c>returnDelimiters</c> is <c>false</c>, delimiter code points serve to separate
+    ///         tokens. A token is a maximal sequence of consecutive code points that are not
+    ///         delimiters.</description></item>
+    ///     <item><description>If <c>returnDelimiters</c> is <c>true</c>, delimiter code points are themselves
+    ///         considered to be tokens. In this case a token will be received for each
+    ///         delimiter code point.</description></item>
+    /// </list>
+    /// <para/>
+    /// A token is thus either one delimiter code point, or a maximal sequence of
+    /// consecutive code points that are not delimiters.
+    /// <para/>
+    /// A <see cref="StringTokenizer"/> object internally maintains a current position
+    /// within the string to be tokenized. Some operations advance this current
+    /// position past the code point processed.
+    /// <para/>
+    /// A token is returned by taking a substring of the string that was used to
+    /// create the <see cref="StringTokenizer"/> object.
+    /// <para/>
+    /// Here's an example of the use of the default delimiter <see cref="StringTokenizer"/>:
+    ///
+    /// <code>
+    /// StringTokenizer st = new StringTokenizer(&quot;this is a test&quot;);
+    /// while (st.MoveNext()) {
+    ///     println(st.Current);
+    /// }
+    /// </code>
+    ///
+    /// <para/>
+    /// This prints the following output: 
+    ///
+    /// <code>
+    ///     this
+    ///     is
+    ///     a
+    ///     test
+    /// </code>
+    ///
+    /// <para/>
+    /// Here's an example of how to use a <see cref="StringTokenizer"/> with a user
+    /// specified delimiter: 
+    ///
+    /// <code>
+    /// StringTokenizer st = new StringTokenizer(
+    ///         &quot;this is a test with supplementary characters \ud800\ud800\udc00\udc00&quot;,
+    ///         &quot; \ud800\udc00&quot;);
+    /// while (st.MoveNext()) {
+    ///     println(st.Current);
+    /// }
+    /// </code>
+    ///
+    /// <para/>
+    /// This prints the following output:
+    ///
+    /// <code>
+    ///     this
+    ///     is
+    ///     a
+    ///     test
+    ///     with
+    ///     supplementary
+    ///     characters
+    ///     \ud800
+    ///     \udc00
+    /// </code>
+    ///
+    /// </remarks>
+    public class StringTokenizer : IEnumerator<string>
+    {
+        private readonly string str;
+        private string delimiters;
+        private readonly bool returnDelimiters;
+        private int position;
+        private int remainingTokens;
+
+        /// <summary>
+        /// Constructs a new <see cref="StringTokenizer"/> for the parameter string using
+        /// whitespace as the delimiter. The <see cref="returnDelimiters"/> flag is set to
+        /// <c>false</c>.
+        /// </summary>
+        /// <param name="str">The string to be tokenized.</param>
+        /// <exception cref="ArgumentNullException">If <paramref name="str"/> is <c>null</c>.</exception>
+        public StringTokenizer(string str)
+            : this(str, " \t\n\r\f", false)
+        { }
+
+        /// <summary>
+        /// Constructs a new <see cref="StringTokenizer"/> for the parameter string using
+        /// the specified delimiters. The <see cref="returnDelimiters"/> flag is set to
+        /// <c>false</c>.
+        /// </summary>
+        /// <param name="str">The string to be tokenized.</param>
+        /// <param name="delimiters">The delimiters to use.</param>
+        /// <exception cref="ArgumentNullException">If <paramref name="str"/> or <paramref name="delimiters"/> is <c>null</c>.</exception>
+        public StringTokenizer(string str, string delimiters)
+            : this(str, delimiters, false)
+        { }
+
+        /// <summary>
+        /// Constructs a new <see cref="StringTokenizer"/> for the parameter <paramref name="str"/> using
+        /// the specified delimiters, returning the delimiters as tokens if the
+        /// parameter <paramref name="returnDelimiters"/> is <c>true</c>.
+        /// </summary>
+        /// <param name="str">The string to be tokenized.</param>
+        /// <param name="delimiters">The delimiters to use.</param>
+        /// <param name="returnDelimiters"><c>true</c> to return each delimiter as a token.</param>
+        /// <exception cref="ArgumentNullException">If <paramref name="str"/> or <paramref name="delimiters"/> is <c>null</c>.</exception>
+        public StringTokenizer(string str, string delimiters, bool returnDelimiters)
+        {
+            this.str = str ?? throw new ArgumentNullException(nameof(str));
+            this.delimiters = delimiters ?? throw new ArgumentNullException(nameof(delimiters));
+            this.returnDelimiters = returnDelimiters;
+            this.position = 0;
+            this.remainingTokens = CountTokens();
+        }
+
+        /// <summary>
+        /// Returns the number of unprocessed tokens remaining in the string.
+        /// </summary>
+        /// <returns>Number of tokens that can be retreived before an 
+        /// <see cref="MoveNext()"/> will return <c>false</c>.</returns>
+        internal int CountTokens()
+        {
+            int count = 0;
+            bool inToken = false;
+            for (int i = position, length = str.Length; i < length; i++)
+            {
+                if (delimiters.IndexOf(str[i], 0) >= 0)
+                {
+                    if (returnDelimiters)
+                        count++;
+                    if (inToken)
+                    {
+                        count++;
+                        inToken = false;
+                    }
+                }
+                else
+                {
+                    inToken = true;
+                }
+            }
+            if (inToken)
+                count++;
+            return count;
+        }
+
+        /// <summary>
+        /// Returns <c>true</c> if unprocessed tokens remain.
+        /// </summary>
+        /// <returns><c>true</c> if unprocessed tokens remain.</returns>
+        internal bool HasMoreTokens()
+        {
+            int length = str.Length;
+            if (position < length)
+            {
+                if (returnDelimiters)
+                    return true; // there is at least one character and even if
+                                 // it is a delimiter it is a token
+
+                // otherwise find a character which is not a delimiter
+                for (int i = position; i < length; i++)
+                    if (delimiters.IndexOf(str[i], 0) == -1)
+                        return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Returns the next token in the string as a <see cref="string"/>.
+        /// </summary>
+        /// <returns>Next token in the string as a <see cref="string"/>.</returns>
+        /// <exception cref="InvalidOperationException">If no tokens remain.</exception>
+        private string NextToken()
+        {
+            int i = position;
+            int length = str.Length;
+
+            if (i < length)
+            {
+                if (returnDelimiters)
+                {
+                    if (delimiters.IndexOf(str[position], 0) >= 0)
+                        return str[position++].ToString();
+                    for (position++; position < length; position++)
+                        if (delimiters.IndexOf(str[position], 0) >= 0)
+                            return str.Substring(i, position - i);
+                    return str.Substring(i);
+                }
+
+                while (i < length && delimiters.IndexOf(str[i], 0) >= 0)
+                    i++;
+                position = i;
+                if (i < length)
+                {
+                    for (position++; position < length; position++)
+                        if (delimiters.IndexOf(str[position], 0) >= 0)
+                            return str.Substring(i, position - i);
+                    return str.Substring(i);
+                }
+            }
+            throw new InvalidOperationException("No more elements");
+        }
+
+        /// <summary>
+        /// Gets the number of remaining tokens in the string
+        /// according to the current delimiters.
+        /// </summary>
+        public int RemainingTokens => remainingTokens;
+
+        /// <summary>
+        /// Gets the current token.
+        /// </summary>
+        public string Current { get; private set; }
+
+        object IEnumerator.Current => Current;
+
+        /// <summary>
+        /// Moves to the next token in the string, or returns <c>false</c>
+        /// if no more tokens remain.
+        /// </summary>
+        /// <returns><c>true</c> if a new token was retrieved; <c>false</c> if there are no remaining tokens.</returns>
+        public bool MoveNext()
+        {
+            if (remainingTokens <= 0)
+                return false;
+
+            Current = NextToken();
+            remainingTokens--;
+            return true;
+        }
+
+        /// <summary>
+        /// Sets the <paramref name="delimiters"/> to a new value, then moves to the next token in the string.
+        /// Returns <c>false</c> if no more tokens remain.
+        /// </summary>
+        /// <param name="delimiters">The new delimiters to use.</param>
+        /// <returns><c>true</c> if a new token was retrieved; <c>false</c> if there are no remaining tokens.</returns>
+        /// <exception cref="ArgumentNullException">If <paramref name="delimiters"/> is <c>null</c>.</exception>
+        public bool MoveNext(string delimiters)
+        {
+            this.delimiters = delimiters ?? throw new ArgumentNullException(nameof(delimiters));
+            this.remainingTokens = CountTokens();
+
+            return MoveNext();
+        }
+
+        void IEnumerator.Reset() => throw new NotSupportedException();
+
+        /// <summary>
+        /// Disposes all resources associated with this object.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// When overridden in a subclass, disposes all resources associated with this object.
+        /// </summary>
+        /// <param name="disposing"><c>true</c> indicates to dispose both managed and
+        /// unmanaged resources. <c>false</c> indicates to dispose only unmanaged resources.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            // nothing to do
+        }
+    }
+}
---
+++ tests/J2N.Tests/Text/TestStringTokenizer.cs
@@ -0,0 +1,369 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace J2N.Text
+{
+    public class TestStringTokenizer : TestCase
+    {
+        /**
+	     * @tests java.util.StringTokenizer#StringTokenizer(java.lang.String)
+	     */
+        [Test]
+        public void Test_ConstructorLjava_lang_String()
+        {
+            // Test for method java.util.StringTokenizer(java.lang.String)
+            assertTrue("Used in tests", true);
+        }
+
+        /**
+         * @tests java.util.StringTokenizer#StringTokenizer(java.lang.String,
+         *        java.lang.String)
+         */
+        [Test]
+        public void Test_ConstructorLjava_lang_StringLjava_lang_String()
+        {
+            // Test for method java.util.StringTokenizer(java.lang.String,
+            // java.lang.String)
+            StringTokenizer st = new StringTokenizer("This:is:a:test:String", ":");
+
+            assertTrue("Created incorrect tokenizer", st.CountTokens() == 5
+                    && st.MoveNext() && (st.Current.Equals("This", StringComparison.Ordinal)));
+        }
+
+        /**
+         * @tests java.util.StringTokenizer#StringTokenizer(java.lang.String,
+         *        java.lang.String, boolean)
+         */
+        [Test]
+        public void Test_ConstructorLjava_lang_StringLjava_lang_StringZ()
+        {
+            // Test for method java.util.StringTokenizer(java.lang.String,
+            // java.lang.String, boolean)
+            StringTokenizer st = new StringTokenizer("This:is:a:test:String", ":",
+                    true);
+            st.MoveNext();
+            assertTrue("Created incorrect tokenizer", st.CountTokens() == 8
+                    && st.MoveNext() && (st.Current.Equals(":", StringComparison.Ordinal)));
+        }
+
+        [Test]
+        public void Test_RemainingTokens()
+        {
+            // Test for method java.util.StringTokenizer(java.lang.String,
+            // java.lang.String, boolean)
+            StringTokenizer st = new StringTokenizer("This:is:a:test:String", ":",
+                    true);
+            st.MoveNext();
+            assertEquals(8, st.RemainingTokens);
+
+            while (st.MoveNext())
+                assertEquals(st.CountTokens(), st.RemainingTokens);
+        }
+
+        /**
+         * @tests java.util.StringTokenizer#countTokens()
+         */
+        [Test]
+        public void Test_countTokens()
+        {
+            // Test for method int java.util.StringTokenizer.countTokens()
+            StringTokenizer st = new StringTokenizer("This is a test String");
+
+            assertEquals("Incorrect token count returned", 5, st.CountTokens());
+        }
+
+        ///**
+        // * @tests java.util.StringTokenizer#hasMoreElements()
+        // */
+        //[Test]
+        //public void test_hasMoreElements()
+        //{
+        //    // Test for method boolean java.util.StringTokenizer.hasMoreElements()
+
+        //    StringTokenizer st = new StringTokenizer("This is a test String");
+        //    st.NextToken();
+        //    assertTrue("hasMoreElements returned incorrect value", st
+        //            .hasMoreElements());
+        //    st.NextToken();
+        //    st.NextToken();
+        //    st.NextToken();
+        //    st.NextToken();
+        //    assertTrue("hasMoreElements returned incorrect value", !st
+        //            .hasMoreElements());
+        //}
+
+        /**
+         * @tests java.util.StringTokenizer#hasMoreTokens()
+         */
+        [Test]
+        public void Test_hasMoreTokens()
+        {
+            // Test for method boolean java.util.StringTokenizer.hasMoreTokens()
+            StringTokenizer st = new StringTokenizer("This is a test String");
+            for (int counter = 0; counter < 5; counter++)
+            {
+                assertTrue(
+                        "StringTokenizer incorrectly reports it has no more tokens",
+                        st.HasMoreTokens());
+                st.MoveNext();
+            }
+            assertTrue("StringTokenizer incorrectly reports it has more tokens",
+                    !st.HasMoreTokens());
+        }
+
+        ///**
+        // * @tests java.util.StringTokenizer#nextElement()
+        // */
+        //[Test]
+        //public void test_nextElement()
+        //{
+        //    // Test for method java.lang.Object
+        //    // java.util.StringTokenizer.nextElement()
+        //    StringTokenizer st = new StringTokenizer("This is a test String");
+        //    assertEquals("nextElement returned incorrect value", "This", ((String)st
+        //            .NextToken()));
+        //    assertEquals("nextElement returned incorrect value", "is", ((String)st
+        //            .NextToken()));
+        //    assertEquals("nextElement returned incorrect value", "a", ((String)st
+        //            .NextToken()));
+        //    assertEquals("nextElement returned incorrect value", "test", ((String)st
+        //            .NextToken()));
+        //    assertEquals("nextElement returned incorrect value", "String", ((String)st
+        //            .NextToken()));
+        //    try
+        //    {
+        //        st.NextToken();
+        //        fail(
+        //                "nextElement failed to throw a NoSuchElementException when it should have been out of elements");
+        //    }
+        //    catch (InvalidOperationException e)
+        //    {
+        //        return;
+        //    }
+        //}
+
+        /**
+         * @tests java.util.StringTokenizer#nextToken()
+         */
+        [Test]
+        public void Test_nextToken()
+        {
+            // Test for method java.lang.String
+            // java.util.StringTokenizer.nextToken()
+            StringTokenizer st = new StringTokenizer("This is a test String");
+
+            st.MoveNext();
+            assertEquals("nextToken returned incorrect value",
+                    "This", st.Current);
+
+            st.MoveNext();
+            assertEquals("nextToken returned incorrect value",
+                    "is", st.Current);
+
+            st.MoveNext();
+            assertEquals("nextToken returned incorrect value",
+                    "a", st.Current);
+
+            st.MoveNext();
+            assertEquals("nextToken returned incorrect value",
+                    "test", st.Current);
+
+            st.MoveNext();
+            assertEquals("nextToken returned incorrect value",
+                    "String", st.Current);
+
+            // Not a valid use case - can never happen in .NET
+//            try
+//            {
+//                st.NextToken();
+//                fail(
+//                        "nextToken failed to throw a NoSuchElementException when it should have been out of elements");
+//            }
+//#pragma warning disable 168
+//            catch (InvalidOperationException e)
+//#pragma warning restore 168
+//            {
+//                return;
+//            }
+        }
+
+        /**
+         * @tests java.util.StringTokenizer#nextToken(java.lang.String)
+         */
+        [Test]
+        public void Test_nextTokenLjava_lang_String()
+        {
+            // Test for method java.lang.String
+            // java.util.StringTokenizer.nextToken(java.lang.String)
+            StringTokenizer st = new StringTokenizer("This is a test String");
+
+            st.MoveNext(" ");
+            assertEquals("nextToken(String) returned incorrect value with normal token String",
+                    "This", st.Current);
+
+            st.MoveNext("tr");
+            assertEquals("nextToken(String) returned incorrect value with custom token String",
+                    " is a ", st.Current);
+
+            st.MoveNext();
+            assertEquals("calling nextToken() did not use the new default delimiter list",
+                    "es", st.Current);
+        }
+
+        //[Test]
+        //public void test_hasMoreElements_NPE()
+        //{
+        //    StringTokenizer stringTokenizer = new StringTokenizer(new String(),
+        //            (String)null, true);
+        //    try
+        //    {
+        //        stringTokenizer.HasMoreElements();
+        //        fail("should throw NullPointerException");
+        //    }
+        //    catch (NullPointerException e)
+        //    {
+        //        // Expected
+        //    }
+
+        //    stringTokenizer = new StringTokenizer(new String(), (String)null);
+        //    try
+        //    {
+        //        stringTokenizer.hasMoreElements();
+        //        fail("should throw NullPointerException");
+        //    }
+        //    catch (NullPointerException e)
+        //    {
+        //        // Expected
+        //    }
+        //}
+
+            // Not valid - in .NET we throw exceptions based on null input, we don't
+            // throw exceptions based on normal workflow
+//        [Test]
+//        public void Test_hasMoreTokens_NPE()
+//        {
+//            StringTokenizer stringTokenizer = new StringTokenizer("",
+//                    (String)null, true);
+//            try
+//            {
+//                stringTokenizer.HasMoreTokens();
+//                fail("should throw NullPointerException");
+//            }
+//#pragma warning disable 168
+//            catch (ArgumentNullException e)
+//#pragma warning restore 168
+//            {
+//                // Expected
+//            }
+
+//            stringTokenizer = new StringTokenizer("", (String)null);
+//            try
+//            {
+//                stringTokenizer.HasMoreTokens();
+//                fail("should throw NullPointerException");
+//            }
+//#pragma warning disable 168
+//            catch (ArgumentNullException e)
+//#pragma warning restore 168
+//            {
+//                // Expected
+//            }
+//        }
+
+        //[Test]
+        //public void test_nextElement_NPE()
+        //{
+        //    StringTokenizer stringTokenizer = new StringTokenizer(new string(),
+        //            (String)null, true);
+        //    try
+        //    {
+        //        stringTokenizer.NextToken();
+        //        fail("should throw NullPointerException");
+        //    }
+        //    catch (ArgumentNullException e)
+        //    {
+        //        // Expected
+        //    }
+
+        //    stringTokenizer = new StringTokenizer(new String(), (String)null);
+        //    try
+        //    {
+        //        stringTokenizer.NextToken();
+        //        fail("should throw NullPointerException");
+        //    }
+        //    catch (ArgumentNullException e)
+        //    {
+        //        // Expected
+        //    }
+        //}
+
+        [Test]
+        public void Test_nextToken_NPE()
+        {
+            StringTokenizer stringTokenizer;
+            try
+            {
+                stringTokenizer = new StringTokenizer("",
+                    (String)null, true);
+                //stringTokenizer.NextToken();
+                fail("should throw NullPointerException");
+            }
+#pragma warning disable 168
+            catch (ArgumentNullException e)
+#pragma warning restore 168
+            {
+                // Expected
+            }
+
+            
+            try
+            {
+                stringTokenizer = new StringTokenizer("", (String)null);
+                //stringTokenizer.NextToken();
+                fail("should throw NullPointerException");
+            }
+#pragma warning disable 168
+            catch (ArgumentNullException e)
+#pragma warning restore 168
+            {
+                // Expected
+            }
+        }
+
+        [Test]
+        public void Test_nextTokenLjava_lang_String_NPE()
+        {
+            StringTokenizer stringTokenizer = new StringTokenizer("");
+            try
+            {
+                stringTokenizer.MoveNext(null);
+                fail("should throw NullPointerException");
+            }
+#pragma warning disable 168
+            catch (ArgumentNullException e)
+#pragma warning restore 168
+            {
+                // Expected
+            }
+        }
+
+        /**
+         * Sets up the fixture, for example, open a network connection. This method
+         * is called before a test is executed.
+         */
+        public override void SetUp()
+        {
+            base.SetUp();
+        }
+
+        /**
+         * Tears down the fixture, for example, close a network connection. This
+         * method is called after a test is executed.
+         */
+        public override void TearDown()
+        {
+            base.TearDown();
+        }
+
+    }
+}
