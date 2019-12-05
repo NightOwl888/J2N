edd6f639c0587bfd142e2c99226c7a33329a0f59
Shad Storhaug <shad@shadstorhaug.com>
2019-12-05 21:24:41 +0700

    Added MathExtensions + tests

---
+++ src/J2N/MathExtensions.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace J2N
+{
+    /// <summary>
+    /// Additions to <see cref="System.Math"/>, implemented as extension methods.
+    /// </summary>
+    public static class MathExtensions
+    {
+        /// <summary>
+        /// Returns the signum function of the specified <see cref="int"/> value. (The
+        /// return value is <c>-1</c> if the specified value is negative; <c>0</c> if the
+        /// specified value is zero; and <c>1</c> if the specified value is positive.)
+        /// <para/>
+        /// This can be useful for testing the results of two <see cref="IComparable{T}.CompareTo(T)"/>
+        /// methods against each other, since only the sign is guaranteed to be the same between implementations.
+        /// </summary>
+        /// <param name="value">The value whose signum has to be computed.</param>
+        /// <returns>The signum function of the specified <see cref="int"/> value.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int Signum(this int value)
+        {
+            // HD, Section 2-7
+            return (value >> 31) | ((-value).TripleShift(31));
+        }
+
+        /// <summary>
+        /// Returns the signum function of the specified <see cref="long"/> value. (The
+        /// return value is <c>-1</c> if the specified value is negative; <c>0</c> if the
+        /// specified value is zero; and <c>1</c> if the specified value is positive.)
+        /// </summary>
+        /// <param name="value">The value whose signum has to be computed.</param>
+        /// <returns>The signum function of the specified <see cref="long"/> value.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int Signum(this long value)
+        {
+            // HD, Section 2-7
+            return (int)((value >> 63) | ((-value).TripleShift(63)));
+        }
+
+        /// <summary>
+        /// Converts an angle measured in degrees to an approximately equivalent angle 
+        /// measured in radians. The conversion from degrees to radians is generally inexact.
+        /// </summary>
+        /// <param name="degrees">An angle in degrees to convert to radians</param>
+        /// <returns>The value in radians</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static double ToRadians(this double degrees)
+        {
+            return degrees / 180 * Math.PI;
+        }
+
+        /// <summary>
+        /// Converts an angle measured in degrees to an approximately equivalent angle 
+        /// measured in radians. The conversion from degrees to radians is generally inexact.
+        /// </summary>
+        /// <param name="degrees">An angle in degrees to convert to radians</param>
+        /// <returns>The value in radians</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static decimal ToRadians(this decimal degrees)
+        {
+            return degrees / 180 * (decimal)Math.PI;
+        }
+
+        /// <summary>
+        /// Converts an angle measured in degrees to an approximately equivalent angle 
+        /// measured in radians. The conversion from degrees to radians is generally inexact.
+        /// </summary>
+        /// <param name="degrees">An angle in degrees to convert to radians</param>
+        /// <returns>The value in radians</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static double ToRadians(this int degrees)
+        {
+            return ((double)degrees) / 180 * Math.PI;
+        }
+
+        /// <summary>
+        /// Converts an angle measured in radians to an approximately equivalent angle 
+        /// measured in degrees. The conversion from radians to degrees is generally 
+        /// inexact; users should not expect Cos((90.0).ToRadians()) to exactly equal 0.0.
+        /// </summary>
+        /// <param name="radians">An angle in radians to convert to radians</param>
+        /// <returns>The value in radians</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static double ToDegrees(this double radians)
+        {
+            return radians * 180 / Math.PI;
+        }
+
+        /// <summary>
+        /// Converts an angle measured in radians to an approximately equivalent angle 
+        /// measured in degrees. The conversion from radians to degrees is generally 
+        /// inexact; users should not expect Cos((90.0).ToRadians()) to exactly equal 0.0.
+        /// </summary>
+        /// <param name="radians">An angle in radians to convert to radians</param>
+        /// <returns>The value in radians</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static decimal ToDegrees(this decimal radians)
+        {
+            return radians * 180 / (decimal)Math.PI;
+        }
+
+        /// <summary>
+        /// Converts an angle measured in radians to an approximately equivalent angle 
+        /// measured in degrees. The conversion from radians to degrees is generally 
+        /// inexact; users should not expect Cos((90.0).ToRadians()) to exactly equal 0.0.
+        /// </summary>
+        /// <param name="radians">An angle in radians to convert to radians</param>
+        /// <returns>The value in radians</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static double ToDegrees(this int radians)
+        {
+            return ((double)radians) * 180 / Math.PI;
+        }
+    }
+}
---
+++ tests/J2N.Tests/TestMathExtensions.cs
@@ -0,0 +1,125 @@
+﻿using NUnit.Framework;
+
+namespace J2N
+{
+    public class TestMathExtensions : TestCase
+    {
+        /**
+         * @tests java.lang.Integer#signum(int)
+         */
+        [Test]
+        public void Test_Signum_Int32()
+        {
+            for (int i = -128; i < 0; i++)
+            {
+                assertEquals(-1, MathExtensions.Signum(i));
+            }
+            assertEquals(0, MathExtensions.Signum(0));
+            for (int i = 1; i <= 127; i++)
+            {
+                assertEquals(1, MathExtensions.Signum(i));
+            }
+        }
+
+        /**
+         * @tests java.lang.Long#signum(long)
+         */
+        [Test]
+        public void Test_Signum_Int64()
+        {
+            for (int i = -128; i < 0; i++)
+            {
+                assertEquals(-1, MathExtensions.Signum(i));
+            }
+            assertEquals(0, MathExtensions.Signum(0));
+            for (int i = 1; i <= 127; i++)
+            {
+                assertEquals(1, MathExtensions.Signum(i));
+            }
+        }
+
+        /**
+         * @tests java.lang.Math#toRadians(double)
+         */
+        [Test]
+        public void Test_ToRadians_Double()
+        {
+            for (double d = 500; d >= 0; d -= 1.0)
+            {
+                double converted = MathExtensions.ToDegrees(MathExtensions.ToRadians(d));
+                assertTrue("Converted number not equal to original. d = " + d,
+                        converted >= d * 0.99999999 && converted <= d * 1.00000001);
+            }
+        }
+
+        /**
+         * @tests java.lang.Math#toDegrees(double)
+         */
+        [Test]
+        public void Test_ToDegrees_Double()
+        {
+            for (double d = 500; d >= 0; d -= 1.0)
+            {
+                double converted = MathExtensions.ToRadians(MathExtensions.ToDegrees(d));
+                assertTrue("Converted number not equal to original. d = " + d,
+                        converted >= d * 0.99999999 && converted <= d * 1.00000001);
+            }
+        }
+
+        /**
+         * @tests java.lang.Math#toRadians(double)
+         */
+        [Test]
+        public void Test_ToRadians_Decimal()
+        {
+            for (decimal d = 500; d >= 0; d -= 1.0M)
+            {
+                decimal converted = MathExtensions.ToDegrees(MathExtensions.ToRadians(d));
+                assertTrue("Converted number not equal to original. d = " + d,
+                        converted >= d * 0.99999999M && converted <= d * 1.00000001M);
+            }
+        }
+
+        /**
+         * @tests java.lang.Math#toDegrees(double)
+         */
+        [Test]
+        public void Test_ToDegrees_Decimal()
+        {
+            for (decimal d = 500; d >= 0; d -= 1.0M)
+            {
+                decimal converted = MathExtensions.ToRadians(MathExtensions.ToDegrees(d));
+                assertTrue("Converted number not equal to original. d = " + d,
+                        converted >= d * 0.99999999M && converted <= d * 1.00000001M);
+            }
+        }
+
+        /**
+         * @tests java.lang.Math#toRadians(double)
+         */
+        [Test]
+        public void Test_ToRadians_Int32()
+        {
+            for (int d = 500; d >= 0; d -= 1)
+            {
+                double converted = MathExtensions.ToDegrees(MathExtensions.ToRadians(d));
+                assertTrue("Converted number not equal to original. d = " + d,
+                        converted >= d * 0.99999999 && converted <= d * 1.00000001);
+            }
+        }
+
+        /**
+         * @tests java.lang.Math#toDegrees(double)
+         */
+        [Test]
+        public void Test_ToDegrees_Int32()
+        {
+            for (int d = 500; d >= 0; d -= 1)
+            {
+                double converted = MathExtensions.ToRadians(MathExtensions.ToDegrees(d));
+                assertTrue("Converted number not equal to original. d = " + d,
+                        converted >= d * 0.99999999 && converted <= d * 1.00000001);
+            }
+        }
+    }
+}
