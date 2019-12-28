65369ecc796be9b62af2b054d19f93824ae84416
Shad Storhaug <shad@shadstorhaug.com>
2019-12-29 03:40:42 +0700

    BUG: Fixed issue with current culture/current UI culture - These properties can change at runtime, so we need to read them on the fly, rather than set them in the constructor.

--- tests/J2N.Tests/Text/TestStringFormatter.cs
+++ tests/J2N.Tests/Text/TestStringFormatter.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using J2N.Globalization;
+using NUnit.Framework;
 using System.Collections.Generic;
 using System.Globalization;
 
@@ -48,6 +49,91 @@ namespace J2N.Text
 
             // NOTE: The cast to object here is required in order to force string.Format to call the right overload.
             Assert.AreEqual(arrayExpected, string.Format(StringFormatter.InvariantCulture, "{0}", (object)array));
+        }
+
+        [Test]
+        public void TestCurrentCultureChange()
+        {
+            var map = new Dictionary<string, IDictionary<int, double>>
+            {
+                { "first", new Dictionary<int, double> { { 1, 1.23 }, { 2, 2.23 }, { 3, 3.23 } } },
+                { "second", new Dictionary<int, double> { { 4, 1.24 }, { 5, 2.24 }, { 6, 3.24 } } },
+                { "third", new Dictionary<int, double> { { 7, 1.25 }, { 8, 2.25 }, { 9, 3.25 } } },
+            };
+            var mapExpectedPortuguese = "{first={1=1,23, 2=2,23, 3=3,23}, second={4=1,24, 5=2,24, 6=3,24}, third={7=1,25, 8=2,25, 9=3,25}}";
+            var mapExpectedUSEnglish = "{first={1=1.23, 2=2.23, 3=3.23}, second={4=1.24, 5=2.24, 6=3.24}, third={7=1.25, 8=2.25, 9=3.25}}";
+
+            var formatter = StringFormatter.CurrentCulture;
+
+            using (var context = new CultureContext("en-US"))
+            {
+                Assert.AreEqual(mapExpectedUSEnglish, string.Format(formatter, "{0}", map));
+            }
+            using (var context = new CultureContext("pt"))
+            {
+                Assert.AreEqual(mapExpectedPortuguese, string.Format(formatter, "{0}", map));
+            }
+        }
+
+        [Test]
+        public void TestCurrentUICultureChange()
+        {
+            var map = new Dictionary<string, IDictionary<int, double>>
+            {
+                { "first", new Dictionary<int, double> { { 1, 1.23 }, { 2, 2.23 }, { 3, 3.23 } } },
+                { "second", new Dictionary<int, double> { { 4, 1.24 }, { 5, 2.24 }, { 6, 3.24 } } },
+                { "third", new Dictionary<int, double> { { 7, 1.25 }, { 8, 2.25 }, { 9, 3.25 } } },
+            };
+            var mapExpectedPortuguese = "{first={1=1,23, 2=2,23, 3=3,23}, second={4=1,24, 5=2,24, 6=3,24}, third={7=1,25, 8=2,25, 9=3,25}}";
+            var mapExpectedUSEnglish = "{first={1=1.23, 2=2.23, 3=3.23}, second={4=1.24, 5=2.24, 6=3.24}, third={7=1.25, 8=2.25, 9=3.25}}";
+
+            var formatter = StringFormatter.CurrentUICulture;
+
+            using (var context = new CultureContext("zh", "en-US"))
+            {
+                Assert.AreEqual(mapExpectedUSEnglish, string.Format(formatter, "{0}", map));
+            }
+            using (var context = new CultureContext("zh", "pt"))
+            {
+                Assert.AreEqual(mapExpectedPortuguese, string.Format(formatter, "{0}", map));
+            }
+        }
+
+        [Test]
+        public void TestNegativeZero_Float()
+        {
+            assertEquals("-0.0", string.Format(StringFormatter.InvariantCulture, "{0}", -0.0f));
+            assertEquals("0.0", string.Format(StringFormatter.InvariantCulture, "{0}", 0.0f));
+        }
+
+        [Test]
+        public void TestNegativeZero_Double()
+        {
+            assertEquals("-0.0", string.Format(StringFormatter.InvariantCulture, "{0}", -0.0d));
+            assertEquals("0.0", string.Format(StringFormatter.InvariantCulture, "{0}", 0.0d));
+        }
+
+        [Test]
+        public void TestDecimalPlaces_Float()
+        {
+            // J2N TODO: The observed behavior for this test in Java 8 is "13.987755". However, we see
+            // 7 decimal places in Lucene.NET tests. Not sure which is correct, but leaving alone as it works in Lucene.NET now.
+            // But need to revisit to find out why the behavior is different between Java 6 (Lucene) and Java 8 (ICU4J) tests.
+            //assertEquals("13.9876543", string.Format(StringFormatter.InvariantCulture, "{0}", 13.9876543210987f));
+            assertEquals("22.0", string.Format(StringFormatter.InvariantCulture, "{0}", 22f));
+        }
+
+        [Test]
+        public void TestDecimalPlaces_Double()
+        {
+            assertEquals("22.0", string.Format(StringFormatter.InvariantCulture, "{0}", 22d));
+        }
+
+        [Test]
+        public void TestBoolean()
+        {
+            assertEquals("true", string.Format(StringFormatter.InvariantCulture, "{0}", true));
+            assertEquals("false", string.Format(StringFormatter.InvariantCulture, "{0}", false));
         }
     }
 }
