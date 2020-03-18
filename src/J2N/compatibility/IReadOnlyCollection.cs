2721e776ae2ae970a92b7ed206b029f9fdcd9809
Dmitry Chubrick <dima.chubrick@duallab.com>
2020-03-18 10:57:11 +0300

    Added .NETFramework 4.0 compatibility classes

---
+++ src/J2N/compatibility/IReadOnlyCollection.cs
@@ -0,0 +1,19 @@
+﻿#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+#if NET40
+
+// https://github.com/dotnet/corefx/blob/48363ac826ccf66fbe31a5dcb1dc2aab9a7dd768/src/Common/src/CoreLib/System/Collections/Generic/IReadOnlyCollection.cs
+
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace System.Collections.Generic
+{
+    // Provides a read-only, covariant view of a generic list.
+    public interface IReadOnlyCollection<out T> : IEnumerable<T>
+    {
+        int Count { get; }
+    }
+}
+
+#endif
---
+++ src/J2N/compatibility/MethodImplAttribute.cs
@@ -0,0 +1,35 @@
+﻿#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+#if NET40
+
+// https://github.com/dotnet/corefx/blob/48363ac826ccf66fbe31a5dcb1dc2aab9a7dd768/src/Common/src/CoreLib/System/Runtime/CompilerServices/MethodImplAttribute.cs
+
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace System.Runtime.CompilerServices
+{
+    // Custom attribute to specify additional method properties.
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Constructor, Inherited = false)]
+    public sealed class MethodImplAttribute : Attribute
+    {
+        public MethodCodeType MethodCodeType;
+
+        public MethodImplAttribute(MethodImplOptions methodImplOptions)
+        {
+            Value = methodImplOptions;
+        }
+
+        public MethodImplAttribute(short value)
+        {
+            Value = (MethodImplOptions)value;
+        }
+
+        public MethodImplAttribute()
+        {
+        }
+
+        public MethodImplOptions Value { get; }
+    }
+}
+#endif
