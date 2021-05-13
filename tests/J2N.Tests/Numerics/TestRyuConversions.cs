1cbd4252109b9bb73477b34c504b8cc039f2eb3f
Shad Storhaug <shad@shadstorhaug.com>
2021-05-13 14:51:07 +0700

    J2N.Tests.Numerics.TestRyuConversions: Added tests comparing against FloatingDecimal

--- tests/J2N.Tests/Numerics/TestRyuConversions.cs
+++ tests/J2N.Tests/Numerics/TestRyuConversions.cs
@@ -1,8 +1,11 @@
 ﻿using NUnit.Framework;
+using RandomizedTesting.Generators;
+using System;
 
 namespace J2N.Numerics {
     public class TestRyuConversions : TestCase {
-        
+
+        private const int NUM_RANDOM_TESTS = 100000;
 
         [Test]
         public void DecimalOnly() {
@@ -82,5 +85,76 @@ namespace J2N.Numerics {
             assertEquals("1.0E-36", s);
         }
 
+        [Test]
+        public void TestToJavaFormatStringFloatFixed_AgainstOldFloatingDecimalForTest()
+        {
+            float[] f = new float[] {
+                -9.8784166e8f, // dtoa() fast path
+                0.70443946f,   // dtoa() fast iterative - int
+                1.8254228e37f  // dtoa() slow iterative
+            };
+
+            for (int i = 0; i < f.Length; i++)
+            {
+                OldFloatingDecimalForTest ofd = new OldFloatingDecimalForTest(f[i]);
+                assertEquals($"Original value: {f[i].ToString("R")} or {f[i].ToHexString()} hexadecimal", expected: ofd.toJavaFormatString(), RyuConversion.FloatToString(f[i]));
+            }
+        }
+
+        [Test]
+        public void TestToJavaFormatStringFloatRandom_AgainstOldFloatingDecimalForTest()
+        {
+            for (int i = 0; i < NUM_RANDOM_TESTS; i++)
+            {
+                float[] f = new float[] {
+                    Random.Next(),
+                    (float)Random.NextGaussian(),
+                    Random.NextSingle()*float.MaxValue
+                };
+                for (int j = 0; j < f.Length; j++)
+                {
+                    OldFloatingDecimalForTest ofd = new OldFloatingDecimalForTest(f[j]);
+                    assertEquals($"Original value: {f[j].ToString("R")} or {f[j].ToHexString()} hexadecimal", expected: ofd.toJavaFormatString(), RyuConversion.FloatToString(f[j]));
+                }
+            }
+        }
+
+        [Test]
+        public void TestToJavaFormatStringFloatFixed_AgainstFloatingDecimal()
+        {
+            float[] f = new float[] {
+                -9.8784166e8f, // dtoa() fast path
+                0.70443946f,   // dtoa() fast iterative - int
+                1.8254228e37f  // dtoa() slow iterative
+            };
+
+            for (int i = 0; i < f.Length; i++)
+            {
+                assertEquals($"Original value: {f[i].ToString("R")} or {f[i].ToHexString()} hexadecimal", expected: FloatingDecimal.ToJavaFormatString(f[i]), actual: RyuConversion.FloatToString(f[i]));
+
+                // Check for round-trip
+                assertEquals($"Failed to round trip: {f[i].ToString("R")} or {f[i].ToHexString()} hexadecimal", BitConversion.SingleToRawInt32Bits(f[i]), BitConversion.SingleToRawInt32Bits(FloatingDecimal.ParseFloat(RyuConversion.FloatToString(f[i]))));
+            }
+        }
+
+        [Test]
+        public void TestToJavaFormatStringFloatRandom_AgainstFloatingDecimal()
+        {
+            for (int i = 0; i < NUM_RANDOM_TESTS; i++)
+            {
+                float[] f = new float[] {
+                    Random.Next(),
+                    (float)Random.NextGaussian(),
+                    Random.NextSingle()*float.MaxValue
+                };
+                for (int j = 0; j < f.Length; j++)
+                {
+                    assertEquals($"Original value: {f[j].ToString("R")} or {f[j].ToHexString()} hexadecimal", expected: FloatingDecimal.ToJavaFormatString(f[j]), actual: RyuConversion.FloatToString(f[j]));
+
+                    // Check for round-trip
+                    assertEquals($"Failed to round trip: {f[j].ToString("R")} or {f[j].ToHexString()} hexadecimal", BitConversion.SingleToRawInt32Bits(f[j]), BitConversion.SingleToRawInt32Bits(FloatingDecimal.ParseFloat(RyuConversion.FloatToString(f[j]))));
+                }
+            }
+        }
     }
 }
