5bd94fbda20246302ff7a6b3bde15e707f97cbe0
Shad Storhaug <shad@shadstorhaug.com>
2021-05-15 00:16:48 +0700

    WIP SlowConversion (Ryu)

---
+++ tests/J2N.Tests/Numerics/SlowFloatTest.cs
@@ -0,0 +1,16 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace J2N.Numerics
+//{
+//    public class SlowFloatTest : FloatToStringTest
+//    {
+//        public override string f(float value, RoundingMode roundingMode)
+//        {
+//            return SlowConversion.floatToString(value, roundingMode);
+//        }
+//    }
+//}
