f567a43838940cd977b801abbfee66b2d684c5ce
Shad Storhaug <shad@shadstorhaug.com>
2021-06-14 18:06:30 +0700

    WIP Benchmarks

---
+++ J2N.Benchmarks/BenchmarkCharacter.cs
@@ -0,0 +1,51 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace J2N.Benchmarks
+{
+    [MemoryDiagnoser]
+    public class BenchmarkCharacter
+    {
+        public static int Iterations = 100000;
+
+        [Benchmark]
+        public void Digit_Ascii_Hexadecimal_Calculated()
+        {
+            for (int i = 0; i < Iterations; i++)
+            {
+                for (int c = 0; c < 256; c++)
+                {
+                    var _ = Character.Digit(c, 16);
+                }
+            }
+        }
+
+        //[Benchmark]
+        //public void Digit_Ascii_Hexadecimal_ReadOnlySpanLookup()
+        //{
+        //    for (int i = 0; i < Iterations; i++)
+        //    {
+        //        for (int c = 0; c < 256; c++)
+        //        {
+        //            var _ = Character.Digit2(c, 16);
+        //        }
+        //    }
+        //}
+
+        //[Benchmark]
+        //public void Digit_Ascii_Hexadecimal_StaticArrayLookup()
+        //{
+        //    for (int i = 0; i < Iterations; i++)
+        //    {
+        //        for (int c = 0; c < 256; c++)
+        //        {
+        //            var _ = Character.Digit3(c, 16);
+        //        }
+        //    }
+        //}
+    }
+}
---
+++ J2N.Benchmarks/Program.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+
+namespace J2N.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<BenchmarkCharacter>();
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
