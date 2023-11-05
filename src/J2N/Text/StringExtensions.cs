41d0e25d5b99d7f5c41712a4c2a75a81e9611994
Shad Storhaug <shad@shadstorhaug.com>
2023-11-05 10:11:53 +0700

    PERFORMANCE: J2N.Text.StringExtensions::LastIndexOf(): Eliminated array allocation in call to Character.ToChars()

--- src/J2N/Text/StringExtensions.cs
+++ src/J2N/Text/StringExtensions.cs
@@ -711,13 +711,14 @@ namespace J2N.Text
         /// <summary>
         /// Handles (rare) calls of lastIndexOf with a supplementary character.
         /// </summary>
+#if FEATURE_METHODIMPLOPTIONS_AGRESSIVEINLINING
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif 
         private static int LastIndexOfSupplementary(this string text, int codePoint, int startIndex)
         {
             if (Character.IsValidCodePoint(codePoint))
             {
-                char[] pair = Character.ToChars(codePoint);
-                char hi = pair[0];
-                char lo = pair[1];
+                Character.ToChars(codePoint, out char hi, out char lo); // J2N: Eliminated array allocation
                 int i = Math.Min(startIndex, text.Length - 2);
                 for (; i >= 0; i--)
                 {
