acf192a5fbd31f70dbb2e6213884e974cecf04ec
Shad Storhaug <shad@shadstorhaug.com>
2023-12-29 05:59:10 +0700

    J2N.Text.StringExtensions, J2N.Memory.MemoryExtensions: Added optimized ReverseText() methods to reverse strings while respecting the correct order of surrogate pairs, just like the StringBuilderExtensions.Reverse() method.

--- src/J2N/Text/StringExtensions.cs
+++ src/J2N/Text/StringExtensions.cs
@@ -30,6 +30,8 @@ namespace J2N.Text
     /// </summary>
     public static class StringExtensions
     {
+        private const int CharStackBufferSize = 64;
+
         #region AsCharSequence
 
         /// <summary>
@@ -996,6 +998,69 @@ namespace J2N.Text
         }
 
         #endregion
+
+        #region ReverseText
+
+        /// <summary>
+        /// Reverses the character sequence and returns a new string.
+        /// If there are any surrogate pairs included in the
+        /// sequence, these are treated as single characters for the
+        /// reverse operation. Thus, the order of the high-low surrogates
+        /// is never reversed.
+        /// <para/>
+        /// Let <c>n</c> be the character length of this character sequence
+        /// (not the length in <see cref="char"/> values) just prior to
+        /// execution of the <see cref="ReverseText(string)"/> method. Then the
+        /// character at index <c>k</c> in the new character sequence is
+        /// equal to the character at index <c>n-k-1</c> in the old
+        /// character sequence.
+        /// <para/>
+        /// Note that the reverse operation may result in producing
+        /// surrogate pairs that were unpaired low-surrogates and
+        /// high-surrogates before the operation. For example, reversing
+        /// "&#92;uDC00&#92;uD800" produces "&#92;uD800&#92;uDC00" which is
+        /// a valid surrogate pair.
+        /// <para/>
+        /// Usage Note: This is the same operation as
+        /// <see cref="StringBuilderExtensions.Reverse(StringBuilder)"/>
+        /// (derived from Java's StringBuilder.reverse() method) but is more
+        /// efficient because it doesn't allocate a new <see cref="StringBuilder"/>
+        /// instance and will use the stack for strings 64 characters and less.
+        /// </summary>
+        /// <param name="text">This <see cref="string"/>.</param>
+        /// <returns>The reversed string with non-reversed surrogate pairs.</returns>
+        /// <seealso cref="StringBuilderExtensions.Reverse(StringBuilder)"/>
+        /// <seealso cref="J2N.Memory.MemoryExtensions.ReverseText(Span{char})"/>
+        public static string ReverseText(this string text)
+        {
+            int length = text.Length;
+#if FEATURE_SPAN
+            var result = length <= CharStackBufferSize
+                ? new ValueStringBuilder(stackalloc char[length])
+                : new ValueStringBuilder(length);
+#else
+            var result = new StringBuilder(length);
+#endif
+            for (int i = length - 1; i >= 0; i--)
+            {
+                char ch = text[i];
+                if (char.IsLowSurrogate(ch) && i > 0)
+                {
+                    char ch2 = text[i - 1];
+                    if (char.IsHighSurrogate(ch2))
+                    {
+                        result.Append(ch2);
+                        result.Append(ch);
+                        i--;
+                        continue;
+                    }
+                }
+                result.Append(ch);
+            }
+            return result.ToString();
+        }
+
+        #endregion ReverseText
 
         #region StartsWith
 
