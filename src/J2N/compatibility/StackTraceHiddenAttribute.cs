a1608d360e654d862dae77484da513450104a26f
Shad Storhaug <shad@shadstorhaug.com>
2024-12-18 00:24:14 +0700

    Added ThrowHelper class from .NET 9 (minus anything that we don't have a resource entry for or anything that doesn't compile in one of our target frameworks)

---
+++ src/J2N/compatibility/StackTraceHiddenAttribute.cs
@@ -0,0 +1,24 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+#if !FEATURE_STACKTRACEHIDDENATTRIBUTE
+
+using System;
+
+namespace J2N
+{
+    /// <summary>
+    /// Types and Methods attributed with StackTraceHidden will be omitted from the stack trace text shown in StackTrace.ToString()
+    /// and Exception.StackTrace
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Constructor | AttributeTargets.Struct, Inherited = false)]
+    internal class StackTraceHiddenAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StackTraceHiddenAttribute"/> class.
+        /// </summary>
+        public StackTraceHiddenAttribute() { }
+    }
+}
+
+#endif
