2f0bec559c1da255c05a82c3c607e84b91c2c15d
Paul Irwin <paulirwin@gmail.com>
2025-05-29 20:41:42 -0600

    Add link to version of code

--- src/J2N/Collections/Generic/OrderedDictionary.cs
+++ src/J2N/Collections/Generic/OrderedDictionary.cs
@@ -1,5 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
+// https://github.com/dotnet/runtime/blob/v9.0.5/src/libraries/System.Collections/src/System/Collections/Generic/OrderedDictionary.cs
 
 using System;
 using System.Collections;
