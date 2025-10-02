10ec7a478b3ef0184e1028d44ada84f9e02ebd91
Shad Storhaug <shad@shadstorhaug.com>
2025-10-03 02:33:29 +0700

    J2N.Numerics.BitOperation::PopCount(ulong): Had the uint software fallback implementation, changed it to use the ulong implementation.

--- src/J2N/Numerics/BitOperation.cs
+++ src/J2N/Numerics/BitOperation.cs
@@ -149,14 +149,14 @@ namespace J2N.Numerics
 #if FEATURE_NUMERICBITOPERATIONS
             return System.Numerics.BitOperations.PopCount(value);
 #else
-            const uint c1 = 0x_55555555u;
-            const uint c2 = 0x_33333333u;
-            const uint c3 = 0x_0F0F0F0Fu;
-            const uint c4 = 0x_01010101u;
+            const ulong c1 = 0x_55555555_55555555ul;
+            const ulong c2 = 0x_33333333_33333333ul;
+            const ulong c3 = 0x_0F0F0F0F_0F0F0F0Ful;
+            const ulong c4 = 0x_01010101_01010101ul;
 
             value -= (value >> 1) & c1;
             value = (value & c2) + ((value >> 2) & c2);
-            value = (((value + (value >> 4)) & c3) * c4) >> 24;
+            value = (((value + (value >> 4)) & c3) * c4) >> 56;
 
             return (int)value;
 #endif
