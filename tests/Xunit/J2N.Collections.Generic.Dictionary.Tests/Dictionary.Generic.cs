95caf96fc7ae580be72ebebb00adae16b9d4f96e
Shad Storhaug <shad@shadstorhaug.com>
2025-11-12 02:29:35 +0700

    Updated Dictionary<TKey, TValue> to match .NET 10 behavior (Fixes #157) (#159)
    * J2N.Collections.Generic.Dictionary<TKey, TValue>: Upgraded to .NET 10 RC2 implementation (with AlternateLookup)
    
    * J2N.Collections.Dictionary<TKey, TValue>.AlternateLookup<TAlternate>: Addressed lookups of null keys
    
    * J2N.Runtime.InteropServices.CollectionMarshal: Added GetValueRefOrNullRef() and GetValueRefOrAddDefault() for the alternate comparer on Dictionary<TKey, TValue>
    
    * J2N.Collections.Generic.Dictionary.Tests: Synced with .NET 10 RC 2

--- tests/Xunit/J2N.Collections.Generic.Dictionary.Tests/Dictionary.Generic.cs
+++ tests/Xunit/J2N.Collections.Generic.Dictionary.Tests/Dictionary.Generic.cs
@@ -1,4 +1,5 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+﻿// Source: https://github.com/dotnet/runtime/blob/v10.0.0-rc.2.25502.107/src/libraries/System.Collections/tests/Generic/Dictionary/Dictionary.Generic.cs
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
