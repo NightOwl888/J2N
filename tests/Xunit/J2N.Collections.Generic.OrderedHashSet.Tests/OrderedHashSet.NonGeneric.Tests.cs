989f6e388434130b7647d69729755d091d9cf367
Shad Storhaug <shad@shadstorhaug.com>
2025-11-21 01:45:25 +0700

    Added OrderedHashSet.NonGeneric.Tests (derived from HashSet<T>, .NET 10 RC2)

---
+++ tests/Xunit/J2N.Collections.Generic.OrderedHashSet.Tests/OrderedHashSet.NonGeneric.Tests.cs
@@ -0,0 +1,38 @@
+﻿// Source: https://github.com/dotnet/runtime/blob/v10.0.0-rc.2.25502.107/src/libraries/System.Collections/tests/Generic/HashSet/HashSet.NonGeneric.Tests.cs
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using J2N.Collections.Generic;
+using System;
+using System.Reflection;
+using System.Runtime.Serialization;
+using Xunit;
+using SCG = System.Collections.Generic;
+
+namespace J2N.Collections.Tests
+{
+    public static class OrderedHashSet_NonGeneric_Tests
+    {
+        // J2N: HashSet_CopyConstructor_ShouldWorkWithRandomizedEffectiveComparer omitted because serialization is not supported
+
+        [Theory]
+        [InlineData(10)]
+        [InlineData(1000)]
+        [InlineData(1000_000)]
+        public static void InsertionOpsOnly_Enumeration_PreservesInsertionOrder(int count)
+        {
+            var set = new HashSet<string>();
+            for (int i = 0; i < count; i++)
+            {
+                set.Add(i.ToString());
+            }
+
+            int j = 0;
+            foreach (string elem in set)
+            {
+                Assert.Equal(j.ToString(), elem);
+                j++;
+            }
+        }
+    }
+}
