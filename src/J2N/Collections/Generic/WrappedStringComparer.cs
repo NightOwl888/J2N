017aaa10d9bf1b00927706229726056246058f4b
Shad Storhaug <shad@shadstorhaug.com>
2025-12-14 16:50:38 +0700

    J2N.Collections.Generic.WrappedStringComparer: Added equality checking for the underlying comparer.

--- src/J2N/Collections/Generic/WrappedStringComparer.cs
+++ src/J2N/Collections/Generic/WrappedStringComparer.cs
@@ -1,4 +1,22 @@
-﻿using System;
+﻿#region Copyright 2019-2025 by Shad Storhaug, Licensed under the Apache License, Version 2.0
+/*  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+#endregion
+
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 
@@ -34,6 +52,16 @@ namespace J2N.Collections.Generic
         // Gets the comparer that should be returned back to the caller when querying the
         // ICollection.Comparer property. Also used for serialization purposes.
         public virtual IComparer<string?> GetUnderlyingComparer() => _underlyingComparer;
+
+        // Equality for the comparer itself.
+        public override bool Equals(object? obj)
+        {
+            // Commonly, both comparers will be the default comparer (and reference-equal). Avoid a virtual method call to Equals() in that case.
+            return _underlyingComparer == obj || _underlyingComparer.Equals(obj);
+        }
+
+        // Equality for the comparer itself.
+        public override int GetHashCode() => _underlyingComparer.GetHashCode();
 
         private sealed class OrdinalComparer : WrappedStringComparer, ISpanAlternateComparer<char, string?>
         {
