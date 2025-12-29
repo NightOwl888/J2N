56abad741115fc212c62c6d3cbb2cc16883e3b39
Shad Storhaug <shad@shadstorhaug.com>
2025-12-30 01:24:46 +0700

    J2N.Collections.Generic.OrderedDictionary<TKey, TValue>: Corrected several documentation references to OrderedDictionary<TKey, TValue> instead of Dictionary<TKey, TValue> (PR feedback)

--- src/J2N/Collections/Generic/OrderedDictionary.cs
+++ src/J2N/Collections/Generic/OrderedDictionary.cs
@@ -254,11 +254,11 @@ namespace J2N.Collections.Generic
         /// <summary>
         /// Returns a read-only <see cref="ReadOnlyDictionary{TKey, TValue}"/> wrapper for the current collection.
         /// </summary>
-        /// <returns>An object that acts as a read-only wrapper around the current <see cref="Dictionary{TKey, TValue}"/>.</returns>
+        /// <returns>An object that acts as a read-only wrapper around the current <see cref="OrderedDictionary{TKey, TValue}"/>.</returns>
         /// <remarks>
-        /// To prevent any modifications to the <see cref="Dictionary{TKey, TValue}"/> object, expose it only through this wrapper.
+        /// To prevent any modifications to the <see cref="OrderedDictionary{TKey, TValue}"/> object, expose it only through this wrapper.
         /// A <see cref="ReadOnlyDictionary{TKey, TValue}"/> object does not expose methods that modify the collection. However,
-        /// if changes are made to the underlying <see cref="Dictionary{TKey, TValue}"/> object, the read-only collection reflects those changes.
+        /// if changes are made to the underlying <see cref="OrderedDictionary{TKey, TValue}"/> object, the read-only collection reflects those changes.
         /// <para/>
         /// This method is an O(1) operation.
         /// </remarks>
@@ -2378,7 +2378,7 @@ namespace J2N.Collections.Generic
                 Dictionary = dictionary;
             }
 
-            /// <summary>Gets the <see cref="Dictionary{TKey, TValue}"/> against which this instance performs operations.</summary>
+            /// <summary>Gets the <see cref="OrderedDictionary{TKey, TValue}"/> against which this instance performs operations.</summary>
             public OrderedDictionary<TKey, TValue> Dictionary { get; }
 
             /// <summary>Gets or sets the value associated with the specified alternate key.</summary>
@@ -2914,7 +2914,7 @@ namespace J2N.Collections.Generic
                 Dictionary = dictionary!; // [!]: asserted above
             }
 
-            /// <summary>Gets the <see cref="Dictionary{TKey, TValue}"/> against which this instance performs operations.</summary>
+            /// <summary>Gets the <see cref="OrderedDictionary{TKey, TValue}"/> against which this instance performs operations.</summary>
             public OrderedDictionary<TKey, TValue> Dictionary { get; }
 
             /// <summary>Gets or sets the value associated with the specified alternate key.</summary>
