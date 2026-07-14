b55a2283ca4e70fe071d8d6c2585576033b6d46a
Shad Storhaug <shad@shadstorhaug.com>
2026-07-14 13:34:57 +0700

    J2N.Text.MutableTextBuffer: Migrated interpolated Append overloads and the AppendInterpolatedStringHandler from upstream into its own partial class file named MutableTextBuffer.Interpolation (commented out, for now)

--- src/J2N/Text/MutableTextBuffer.cs
+++ src/J2N/Text/MutableTextBuffer.cs
@@ -733,35 +733,6 @@ namespace J2N.Text
                 AppendInternal(value.ToString());
         }
 
-
-        // J2N TODO: API - String interpolation for J2N formatters
-
-        ///// <summary>Appends the specified interpolated string to this instance.</summary>
-        ///// <param name="handler">The interpolated string to append.</param>
-        ///// <returns>A reference to this instance after the append operation has completed.</returns>
-        //[CodeGenerationExtensionImplementation]
-        //internal void AppendInternal([InterpolatedStringHandlerArgument("")] ref AppendInterpolatedStringHandler handler) => this;
-
-        ///// <summary>Appends the specified interpolated string to this instance.</summary>
-        ///// <param name="provider">An object that supplies culture-specific formatting information.</param>
-        ///// <param name="handler">The interpolated string to append.</param>
-        ///// <returns>A reference to this instance after the append operation has completed.</returns>
-        //[CodeGenerationExtensionImplementation]
-        //internal void AppendInternal(IFormatProvider? provider, [InterpolatedStringHandlerArgument("", nameof(provider))] ref AppendInterpolatedStringHandler handler) => this;
-
-        ///// <summary>Appends the specified interpolated string followed by the default line terminator to the end of the current MutableTextBuffer object.</summary>
-        ///// <param name="handler">The interpolated string to append.</param>
-        ///// <returns>A reference to this instance after the append operation has completed.</returns>
-        //[CodeGenerationExtensionImplementation]
-        //internal void AppendLineInternal([InterpolatedStringHandlerArgument("")] ref AppendInterpolatedStringHandler handler) => AppendLine();
-
-        ///// <summary>Appends the specified interpolated string followed by the default line terminator to the end of the current MutableTextBuffer object.</summary>
-        ///// <param name="provider">An object that supplies culture-specific formatting information.</param>
-        ///// <param name="handler">The interpolated string to append.</param>
-        ///// <returns>A reference to this instance after the append operation has completed.</returns>
-        //[CodeGenerationExtensionImplementation]
-        //internal void AppendLineInernal(IFormatProvider? provider, [InterpolatedStringHandlerArgument("", nameof(provider))] ref AppendInterpolatedStringHandler handler) => AppendLine();
-
         /// <summary>
         /// Inserts the string representation of a specified Boolean value to this instance
         /// at the specified character position.
