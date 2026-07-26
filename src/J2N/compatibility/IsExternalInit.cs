60f73e671e5e9d3d4e11da70da5ffb228803f5a7
Shad Storhaug <shad@shadstorhaug.com>
2026-07-26 22:32:04 +0700

    IsExternalInit: Fixed typo in the documentation

--- src/J2N/compatibility/IsExternalInit.cs
+++ src/J2N/compatibility/IsExternalInit.cs
@@ -5,7 +5,7 @@ namespace System.Runtime.CompilerServices
     /// This is a compatibility patch for the compiler to support init properties on
     /// older target frameworks. It serves no other purpose and is internal to avoid
     /// conflicting with other libraries. Note that the System.Runtime.CompilerServices
-    /// namespace declaration here is deliverate because that is where the compiler expects
+    /// namespace declaration here is deliberate because that is where the compiler expects
     /// this type to exist.
     /// </summary>
     internal static class IsExternalInit
